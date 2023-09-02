850b98b4417c64e767dbbbfddab529b8095f8c7e
Theodore Abitbol <theodore.abitbol@gmail.com>
2023-09-02 14:52:13 +0200

    first commit

---
+++ my-app/src/components/App.js
@@ -0,0 +1,16 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Premier test JS !
+        </p>
+          Learn React
+      </header>
+    </div>
+  );
+}
+
+export default App;
