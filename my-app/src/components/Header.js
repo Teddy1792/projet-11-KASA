492c41f5f248fb047bffda1fbe8f45ae2e8cf9fc
Theodore Abitbol <theodore.abitbol@gmail.com>
2023-09-12 17:02:08 +0200

    last commit before review

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,25 +1,28 @@
-import { Link, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../styles/Header.scss';
 import logo from '../logo/LOGO.svg';
 
-//https://reactrouter.com/en/main/components/nav-link : pour se passer de isactive
-
 export function Header() {
-    const location = useLocation();
-    const isActive = (path) => location.pathname === path;
-
     return (
         <header>
-            <Link to={"/"}>
+            <NavLink to={"/"}>
                 <img alt="logo du site Kasa" src={logo} />
-            </Link>
+            </NavLink>
             <div className="text-header">
-            <Link to="/" className={isActive('/') ? 'active text-accueil' : 'text-accueil'}>
+                <NavLink 
+                    to="/" 
+                    className="text-accueil" 
+                    activeClassName="active"
+                >
                     Accueil
-                </Link>
-                <Link to="/Apropos" className={isActive('/Apropos') ? 'active text-apropos' : 'text-apropos'}>
+                </NavLink>
+                <NavLink 
+                    to="/Apropos" 
+                    className="text-apropos" 
+                    activeClassName="active"
+                >
                     A Propos
-                </Link>
+                </NavLink>
             </div>
         </header>
     );
